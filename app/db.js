26987921d7291220918811bde73080de88daa627
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-03-26 23:58:47 -0400

    dashboard layout done

---
+++ app/db.js
@@ -0,0 +1,104 @@
+/**
+ * Handles database connections
+ */
+
+var connection = null;
+var r = require('rethinkdbdash')({
+    port: 28015,
+    host: 'localhost'
+}, function(err, conn) {
+    if (err) throw err;
+    connection = conn;
+});
+
+/**DB tables */
+var dbname = "web_db";
+var tbusers = "users";
+var tbgroups= "groups";
+
+/**Setting up the app tables */
+// create user table
+r.dbList().contains(dbname)
+    .do(function(dbExists) {
+        return r.branch(
+            dbExists, {
+                dbs_created: 0
+            },
+            r.dbCreate(dbname)
+        );
+    }).run();
+
+/**Create table users */
+r.db(dbname).tableList().contains(tbusers)
+    .do(function(dbTableExists) {
+        return r.branch(
+            dbTableExists, {
+                tables_created: 0
+            },
+            r.db(dbname).tableCreate(tbusers, {
+                primaryKey: 'email'
+            }));
+    }).run();
+
+/**Create table groups */
+r.db(dbname).tableList().contains(tbgroups)
+    .do(function(dbTableExists) {
+        return r.branch(
+            dbTableExists, {
+                tables_created: 0
+            },
+            r.db(dbname).tableCreate(tbgroups, {
+                primaryKey: 'groupid'
+            }));
+    }).run();
+
+/**USER signup */
+var addUSER = function(data, callback) {
+    //check if email exists
+    r.db(dbname).table(tbusers).get(data.email).run()
+        .then(function(response) {
+            if (response == null) {
+                console.log('user not exists');
+
+                //insert the new user
+                r.db(dbname).table(tbusers).insert({
+                    'email': data.email,
+                    'fname': data.fname,
+                    'lname': data.lname,
+                    'passw': data.passw,
+                    'school': data.school,
+                    'u_img': data.u_img,
+                    'groups': []
+                }).run();
+
+                callback(1);
+            } else {
+                console.log('user found');
+                callback(-1);
+            }
+        }).catch(function(err) {
+            callback(0);
+        });
+};
+
+/**USER  getuser*/
+var getUSER = function(useremail, callback){
+    //get user data based on user email
+    r.db(dbname).table(tbusers).get(useremail).run()
+        .then(function(response){
+           // console.log(response);
+
+            if(response == null){
+                callback(null);
+            }else{
+                callback(response);
+            }
+        }).catch(function(err){
+            console.log(err);
+        });
+}
+
+
+/**Export the modules */
+module.exports.addUSER = addUSER;
+module.exports.getUSER = getUSER;
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var session = require('express-session');
+var dbconn = require('../app/db');
+var encrypt = require('../app/encrypt');
+var univ_list = require('../app/univ_list.json');
+var Identicon= require('identicon.js');
+
+/**Options for indenticon**/
+var Iden_options = {
+    foreground: [Math.floor(Math.random() * 254), Math.floor(Math.random() * 254), Math.floor(Math.random() * 254), 255],    
+    background: [238, 238, 238, 238], 
+    margin: 0.2,    
+    size: 32,               
+    format: 'svg'   
+};
+
 
 /**SET the session*/
 router.use(session({
@@ -45,28 +59,116 @@ router.get('/signup', function(req, res, next){
         alert = req.query.notify;
     }
 
+    //console.log(univ_list);
+
     res.render('signup', {
         title: 'Signup',
-        alert: alert
+        alert: alert,
+        univ_list: univ_list
 	});
 });
 
 
 /**GET user dashboard */
 router.get('/dashboard', function(req, res, next){
+    /**Makesure user session exists */
+    if (req.session.usersess) {
+        username = req.session.name;
+        useremail = req.session.email;
+        uimg = req.session.uimg;
 
+        var alert = null;
+
+        if(req.query.notify != null){
+            alert = req.query.notify;
+        }
+
+        res.render('dashboard', {
+            title: 'Dashboard',
+            name: username,
+            uemail: useremail,
+            uimg: uimg,
+            alert: alert
+        });
+    } else {
+        res.redirect('/');
+    }
 });
 
 
 /**POST user signin */
 router.post('/user_signin', function(req, res, next){
+    var email = req.body.strEmail.toLowerCase();
+    var passw = req.body.strPassw;
 
+    dbconn.getUSER(email, function(state){
+        //console.log(state);
+
+        /**check if user exists */
+        if(state != null){
+            if(state.email == email && encrypt.compareHASH(passw, state.passw)){
+                /**Create a user session */
+                sess = req.session;
+                sess.usersess = true;
+                req.session.usersess = true;
+                req.session.email = state.email;
+                req.session.name = state.fname + ' ' + state.lname;
+                req.session.uimg = state.u_img;
+
+                //console.log(req.session);
+                res.redirect('/dashboard');
+            }else{
+                res.redirect('/signin?notify=passw');
+            }
+        }else{
+            res.redirect('/signin?notify=notfound');
+        }
+    });
 });
 
 
 /**POST user signup */
 router.post('/user_signup', function(req, res, next){
+    var email = req.body.strEmail.toLowerCase();
+    var fname = req.body.strFname;
+    var lname = req.body.strLname;
+    var school= req.body.strUniversity;
 
+    var passw1 = req.body.strPassw1;
+    var passw2 = req.body.strPassw2;
+
+    if(passw1 == passw2){
+        //console.log('login');
+
+        //data to insert into user
+        data = {
+            email: email,
+            fname: fname,
+            lname: lname,
+            school: school,
+            u_img: new Identicon(encrypt.passwHASH(email), Iden_options).toString('base64'),
+            passw: encrypt.passwHASH(passw1)
+        }
+
+        console.log(data);
+
+
+        /**Add data to db */
+        dbconn.addUSER(data, function(state){
+            if(state == 1){
+                //console.log('Entered');
+                res.redirect('/signup?notify=success');
+            } else if(state == -1){
+                //console.log('Duplicate');
+                res.redirect('/signup?notify=duplicate');
+            } else{
+                //console.log('Error');
+                res.redirect('/signup?notify=error');
+            }
+        });
+    }else{
+        res.redirect('/signup?notify=passw');
+}
 });
 
 
