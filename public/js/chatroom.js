368b41711ab9afbfcc536c02ec28d818e63639e2
Travis Madill <TravisMadill@gmail.com>
2018-04-03 16:39:16 -0400

    Realtime chat works now

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -11,35 +11,64 @@ firebase.auth().onAuthStateChanged(function(user){
 
 function initializeChatRoom(){
 	let roomCode = $('#roomCode').val();
+
+	// db.ref('chat_groups/' + roomCode + '/messages').orderByChild('postTime').once('value', function(snapshot){
+	// 	snapshot.forEach(function(childSnap){
+	// 		let message = $(document.createElement('div')).addClass('messageContainer');
+	// 		let messageHead = $(document.createElement('div')).addClass('messageHead');
+	// 		let messageBody = $(document.createElement('div')).addClass('messageBody');
+	
+	// 		let dt = new Date(childSnap.val().postTime*1000);
+	// 		messageHead.append(childSnap.val().user + ' [' + dt.toDateString() + ' ' + dt.toTimeString() + ']');
+			
+	// 		messageBody.append(childSnap.val().text);
+			
+	// 		message.append(messageHead);
+	// 		message.append(messageBody);
+	// 		$('#chatLog').append(message);
+	// 	});
+	// });
+
 	db.ref('chat_groups/' + roomCode + '/messages').on('child_added', function(data){
 		let message = $(document.createElement('div')).addClass('messageContainer');
 		let messageHead = $(document.createElement('div')).addClass('messageHead');
 		let messageBody = $(document.createElement('div')).addClass('messageBody');
 
-		let dt = new Date(data.postTime*1000);
-		messageHead.append(data.uid + ' [' + dt.toDateString() + ' ' + dt.toTimeString() + ']');
+		console.log(data.val())
+		let dt = new Date(data.val().postTime*1000);
+		messageHead.append(data.val().user + ' [' + dt.toLocaleDateString() + ' ' + dt.toLocaleTimeString() + ']');
 		
-		messageBody.append(data.text);
+		messageBody.append(data.val().text);
 		
 		message.append(messageHead);
 		message.append(messageBody);
 		$('#chatLog').prepend(message);
 	});
-
-	db.ref('chat_groups/' + roomCode + '/messages').orderByChild('postTime').once('value', function(snapshot){
-		snapshot.forEach(function(childSnap){
-			let message = $(document.createElement('div')).addClass('messageContainer');
-			let messageHead = $(document.createElement('div')).addClass('messageHead');
-			let messageBody = $(document.createElement('div')).addClass('messageBody');
-	
-			let dt = new Date(data.postTime*1000);
-			messageHead.append(data.uid + ' [' + dt.toDateString() + ' ' + dt.toTimeString() + ']');
-			
-			messageBody.append(data.text);
-			
-			message.append(messageHead);
-			message.append(messageBody);
-			$('#chatLog').append(message);
-		});
-	});
 }
+
+function sendMsg(){
+	let msg = $('#chatmsg').val();
+	if(msg != '' && msg != null){
+		let roomCode = $('#roomCode').val();
+		let dt = Math.round(new Date().getTime()/1000.0);
+		db.ref('chat_groups/' + roomCode + '/messages').push({
+			user: curUser.uid,
+			text: msg,
+			postTime: dt
+		})
+		$('#chatmsg').val('');
+	}
+}
+
+$('#chatmsg').on('keydown', function(e){
+	switch(e.which){
+	case 13:
+		console.log('Enter key pressed.');
+		sendMsg();
+		return false;
+	}
+});
+
+$('#submitMsg').click(function(){
+	sendMsg();
+});
